5f6cf00547e4f3dcfbeb657dcdcdf315246a5113
Rohiths1227 <rohithofficial1227@gmail.com>
2026-03-15 22:28:32 +0530

    Add files via upload

---
+++ Online_Order.js
@@ -0,0 +1,13 @@
+console.log("Order Placed");
+
+setTimeout(function () {
+    console.log("Order Preparing");
+}, 2000);
+
+setTimeout(function () {
+    console.log("Out for Delivery");
+}, 5000);
+
+setTimeout(function () {
+    console.log("Order Delivered");
+}, 8000);
