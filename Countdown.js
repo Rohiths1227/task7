87462705db473c1b96411a78ab99610036c10735
Rohiths1227 <rohithofficial1227@gmail.com>
2026-03-15 22:41:32 +0530

    Add files via upload

---
+++ Countdown.js
@@ -0,0 +1,13 @@
+let time = 10;
+
+let timer = setInterval(function () {
+
+    console.log(time);
+    time--;
+
+    if (time < 0) {
+        clearInterval(timer);
+        console.log("Meeting Started");
+    }
+
+}, 1000);
